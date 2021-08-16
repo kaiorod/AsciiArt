d41919e80717aad80f1a7a05a77aa2c7f6f372fb
kaiorod <caiorodrigo2775@gmail.com>
2021-08-15 23:11:50 -0300

    finaliza AsciiArt

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AsciiArt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("  ||  ||  ");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write("  \\\\");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("()");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("//  ");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write(" //");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("(__)");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("\\\\ ");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine(" ||    || ");
+            Console.ResetColor();
+
+        }
+    }
+}
